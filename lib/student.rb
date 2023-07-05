c7dbea2bcc092d9451cd25fa1bd32eb365b46ba1
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-07-05 15:50:04 +0300

    assignment complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user"
+
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn knowledge
+        @knowledge << knowledge
+    end
 
 end
+
+std1 = Student.new
+std1.first_name = "Adriel"
+puts std1.first_name
+puts std1.last_name
